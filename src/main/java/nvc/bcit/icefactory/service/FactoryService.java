488516edd023e1c11f6f6acf19aab49a58f92061
WasokSriparat <sriparat123@gmail.com>
2022-01-28 15:08:26 +0700

    first commit

---
+++ src/main/java/nvc/bcit/icefactory/service/FactoryService.java
@@ -0,0 +1,25 @@
+package nvc.bcit.icefactory.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import nvc.bcit.icefactory.model.Factory;
+import nvc.bcit.icefactory.repository.FactoryRepository;
+
+@Service
+public class FactoryService {
+    
+    @Autowired
+    FactoryRepository factoryRepository;
+
+    public List<Factory> findAll(){
+        return factoryRepository.findAll();
+    }
+
+    public Factory findById(int id){
+        return factoryRepository.getById(id);
+    }
+
+}
